9c5c435a587768632f2d39f4c3f239e9a65416ac
Alexis Montealegre <ajmu26@gmail.com>
2020-09-07 22:03:08 -0300

    Done

---
+++ app/models/baby.rb
@@ -0,0 +1,28 @@
+class Baby < ApplicationRecord
+    has_many :breastfeedings, dependent: :destroy
+
+    def total_quantity
+        breastfeedings.sum(:quantity)
+    end
+
+    def breastfeeding_time_today
+        breastfeedings.where('created_at > ?', Date.today)
+        
+    end
+
+    def last_breastfeeding_today_date
+        breastfeedings.where('created_at > ?', Date.today).last.created_at
+    end
+
+    def breastfeeding_quantity_today
+        breastfeedings.where('created_at > ?', Date.today).sum( :quantity)
+
+        
+    end
+
+    def breastfeeding_time_today
+        breastfeedings.where('created_at > ?', Date.today).sum( :feeding_time)
+    end
+
+end
+
